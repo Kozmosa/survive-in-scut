e0888b71a7c4de886f25fbf43f7ac3152ecd709e
Kozmosa <xuyang1638@protonmail.com>
2025-09-28 16:17:11 +0000

    [important chores] 警钟长鸣。add gray mode for the site temporarily

--- docs/.vuepress/layouts/Layout.vue
+++ docs/.vuepress/layouts/Layout.vue
@@ -2,6 +2,12 @@
 import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
 </script>
 
+<style>
+html {
+  filter: grayscale(100%);
+}
+</style>
+
 <template>
   <ParentLayout>
     <template #page-bottom>
